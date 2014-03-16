c518f04092857d3e92fb94893b7d694715737519
Kevin LEVRON <kevin.levron@univ-pau.fr>
2014-03-16 14:26:39 +0100

    Initial

---
+++ FontAwesome.php
@@ -0,0 +1,27 @@
+<?php
+namespace soju\yii2fontawesome;
+
+use yii\helpers\Html;
+
+class FontAwesome
+{
+    public static function icon($iconOptions)
+    {
+        if (is_string($iconOptions)) {
+            $icon = $iconOptions;
+            $iconOptions = [];
+        } else if (is_array($iconOptions) && isset($iconOptions[0])) {
+            $icon = $iconOptions[0];
+            unset($iconOptions[0]);
+        } else {
+            return;
+        }
+
+        $options = ['class' => 'fa fa-' . $icon];
+        foreach ($iconOptions as $opt) {
+            Html::addCssClass($options, 'fa-' . $opt);
+        }
+
+        return Html::tag('i', '', $options);
+    }
+}
---
+++ FontAwesomeAsset.php
@@ -0,0 +1,10 @@
+<?php
+namespace soju\yii2fontawesome;
+
+class FontAwesomeAsset extends \yii\web\AssetBundle
+{
+    public $sourcePath = '@vendor/fortawesome/font-awesome';
+    public $css = [
+        'css/font-awesome.min.css',
+    ];
+}
